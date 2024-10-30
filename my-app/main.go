84b8cfe42bc54701e6105d42f2036a1bf536bf32
satoshiakatsuka <tsukasa7613@gmail.com>
2024-10-30 19:21:20 +0900

    変数宣言・列挙型について実装

--- my-app/main.go
+++ my-app/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Fruit int
+type Animal int
+
+const (
+	Apple Fruit = iota
+	Orange
+	Banana
+)
+
+const (
+	Monkey Animal = iota
+	Elephant
+	Pig
+)
+
 func main() {
 	fmt.Println("こんにちは せかい")
+
+	var fruit Fruit = Apple
+	fmt.Println(fruit)
+
+	//fruit = Elephant // Fruit型のfruitにAnimal型のElephantを代入するとエラー
+	fruit = Banana
+	fmt.Println(fruit)
 }
